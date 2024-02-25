374561d81767f6f1eecc1a525ba0ef6e857c04fd
scrsxd <160347178+scrsxd@users.noreply.github.com>
2024-02-25 17:55:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function rollDice() {
+    let random_number1 = Math.floor((Math.random() * 6) + 1)
+    let random_number2 = Math.floor((Math.random() * 6) + 1)
+
+    document.querySelectorAll(".dice")[0].setAttribute("class", "dice")
+    document.querySelectorAll(".dice")[1].setAttribute("class", "dice")
+
+    document.querySelector("body > div > div:nth-child(2) > img").setAttribute("src", "./images/dice"+random_number1+".png")
+    document.querySelector("body > div > div:nth-child(3) > img").setAttribute("src", "./images/dice"+random_number2+".png")
+
+    console.log(random_number1)
+    console.log(random_number2)
+
+    if (random_number1 > random_number2){
+        document.querySelector("body > div.message > p").textContent = "🚩" + document.querySelector("body > div > div:nth-child(2) > p").textContent + " wins!";
+        document.querySelector("body > div > div:nth-child(2) > p").setAttribute("class", "winner")
+        document.querySelector("body > div > div:nth-child(3) > p").setAttribute("class", "loser")
+    }
+    else if (random_number2 > random_number1){
+        document.querySelector("body > div.message > p").textContent = document.querySelector("body > div > div:nth-child(3) > p").textContent + " wins! 🚩";
+        document.querySelector("body > div > div:nth-child(3) > p").setAttribute("class", "winner")
+        document.querySelector("body > div > div:nth-child(2) > p").setAttribute("class", "loser")
+    }
+    else{
+        document.querySelector("body > div.message > p").textContent = "Draw!"
+
+    }
+}
+
+document.querySelector("h1").onclick = rollDice;
